5eb825f1b45dd28b989a0bed6aa4d52088f881d2
Fizioh <batrom@laposte.net>
2023-07-25 23:11:43 +0200

    fix: translate navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           }}>
             <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
             <p className='text-white text-[18px] font-bold cursor-pointer flex'> 
-            Yassine Benosmane &nbsp;<span className='sm:block hidden'>| Fullstack Dev</span></p>
+            Yassine Benosmane &nbsp;<span className='sm:block hidden'>| Dev Fullstack</span></p>
           </Link>
           <ul className='list-none hidden sm:flex flex-row gap-10'>
             {navLinks.map((link) => (
